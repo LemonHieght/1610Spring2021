5669d603184ad683ab8adcc0de1ad3e2ab75fe40
Will lemmons <will.e.lemmons@gmail.com>
2021-01-31 21:16:53 -0900

    skill tree 1.0

---
+++ UVU Truck game/Assets/Scripts/Experiments/SkillButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillButton : MonoBehaviour
+{
+    public Image skillImage;
+    public Text skillNameText;
+    public Text skillDesText;
+    public int skillButtonID;
+
+    public void PressSkillButton()
+    {
+        
+    }
+}

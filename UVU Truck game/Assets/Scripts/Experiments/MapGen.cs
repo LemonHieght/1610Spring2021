b74bb861908e2f6574c092d9d43ff657ae84cb90
Will lemmons <will.e.lemmons@gmail.com>
2021-02-19 15:27:44 -0900

    ProceduralMap1.3

--- UVU Truck game/Assets/Scripts/Experiments/MapGen.cs
+++ UVU Truck game/Assets/Scripts/Experiments/MapGen.cs
@@ -4,20 +4,51 @@ using UnityEngine;
 
 public class MapGen : MonoBehaviour
 {
-    public int mapWith;
+    public int mapWidth;
     public int mapHeight;
     public float noiseScale;
     public int octaves;
+    [Range(0,1)]
     public float persistance;
     public float lacunarity;
+    public int seed;
+    public Vector2 offSet;
     
     public bool autoUpdate;
 
     public void GenerateMap()
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWith, mapHeight, noiseScale, octaves, persistance, lacunarity);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offSet);
 
         MapDisplay display = FindObjectOfType<MapDisplay>();
         display.DrawNoiseMap(noiseMap);
+    }
+
+    void OnValidate()
+    {
+        if (mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+
+        if (mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+
+        if (lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+
+        if (octaves < 1)
+        {
+            octaves = 1;
+        }
+
+        if (noiseScale < 0)
+        {
+            noiseScale = 0;
+        }
     }
 }
--- UVU Truck game/Assets/Scripts/Experiments/Noise.cs
+++ UVU Truck game/Assets/Scripts/Experiments/Noise.cs
@@ -1,12 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public static class Noise 
 {
-    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale, int octaves, float persistance, float lacunarity)
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight,int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offSet)
     {
        float[,] noiseMap = new float[mapWidth, mapHeight];
+       
+       System.Random prng = new System.Random(seed);
+       Vector2[] octaveOffSet = new Vector2[octaves];
+       for (int i = 0; i < octaves; i++)
+       {
+           float offSetX = prng.Next(-100000, 100000) + offSet.x;
+           float offSetY = prng.Next(-100000, 100000) + offSet.y;
+           octaveOffSet [i] = new Vector2(offSetX,offSetY);
+       }
        if (scale <= 0)
        {
            scale = 0.0001f;
@@ -14,6 +24,10 @@ public static class Noise
 
        float maxNoiseheight = float.MinValue;
        float minNoiseHeight = float.MaxValue;
+
+       float halfWidth = mapWidth / 2f;
+       float halfHeight = mapHeight / 2f;
+       
        for (int y = 0; y < mapHeight; y++)
        {
            for (int x = 0; x < mapWidth; x++)
@@ -23,8 +37,8 @@ public static class Noise
                float noiseHeight = 0;
                for(int i = 0; i < octaves; i++)
                {
-                   float sampleX = x / scale * frequency;
-                   float sampleY = y / scale * frequency;
+                   float sampleX = (x - halfWidth) / scale * frequency + octaveOffSet[i].x;
+                   float sampleY = (y - halfHeight) / scale * frequency + octaveOffSet[i].y;
 
                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
                    noiseHeight += perlinValue * amplitude;

0166d7c04833f4c342934b737e159c358dd86672
Will Lemmons <will.e.lemmons@gmail.com>
2021-03-26 12:13:25 -0800

    Prototype2.3

---
+++ UVU Truck game/Assets/Scripts/CreateWithCode/SpawnManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-23, 23), 0, -15);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            
+            Instantiate(animalPrefabs[animalIndex], spawnPos,
+                animalPrefabs[animalIndex].transform.rotation);  
+        }
+    }
+}

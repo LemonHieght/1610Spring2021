32a9ece6e91dbd440d50cdb417795ba43022d3d6
Will Lemmons <will.e.lemmons@gmail.com>
2021-03-29 09:48:27 -0800

    Challenge2

---
+++ UVU Truck game/Assets/Challenge 2/Scripts/Controller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float timer = 1f;
+    public bool shoot = true;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && shoot == true)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            shoot = false;
+
+        }
+
+        else if (shoot == false)
+        {
+            timer--;
+        }
+
+        if (timer <= 0)
+        {
+            shoot = true;
+            timer = 50f;
+        }
+    }
+}

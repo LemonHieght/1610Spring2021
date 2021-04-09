ef49c7d5e942b90943ba64d07edbf422dfdb45ec
Will Lemmons <will.e.lemmons@gmail.com>
2021-04-08 19:52:36 -0800

    Prototype3.1

---
+++ UVU Truck game/Assets/Scripts/CreateWithCode/PlayerJump.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    private Rigidbody rigid;
+    public float jumpForce = 100;
+    public float gravityMod;
+    public bool isGrounded = true;
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityMod;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)&& isGrounded == true)
+        {
+            rigid.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        isGrounded = true;
+    }
+}

35079872c9e674ae2d0b6cd148400403299dcf8a
Will Lemmons <will.e.lemmons@gmail.com>
2021-04-16 12:58:53 -0800

    ProceduralMap2.1.1

--- UVU Truck game/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ UVU Truck game/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
     public Vector3 offset = new Vector3(0, 3, -7);
+    public float turnSpeed = 10f;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +21,8 @@ public class FollowPlayerX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        offset = Quaternion.AngleAxis(Input.GetAxisRaw("Mouse X") * turnSpeed, Vector3.up)  * offset;
         transform.position = plane.transform.position + offset;
+        transform.LookAt(plane.transform.position);
     }
 }
---
+++ UVU Truck game/Assets/Scripts/Experiments/Veiwer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Veiwer : MonoBehaviour
+{
+    private Vector3 player;
+    public float speed = 30f;
+    private Rigidbody rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Debug.Log("Turn off game.");
+        player =new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        
+        Movement(player);
+    }
+
+    void Movement(Vector3 direction)
+    {
+        rigid.MovePosition(transform.position + (direction * speed * Time.deltaTime));
+        if (player != Vector3.zero)
+        {
+            transform.forward = player;
+        }  
+    }
+}
